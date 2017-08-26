155af4601ebe546c4b7302ad4fd9faa3b88c9953
Matt Woods <mattwoods21@gmail.com>
2017-08-26 17:57:22 -0500

    added Landing component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login with Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href="/api/logout">Logout</a>
+					</li>
+				);
+		}
+	}
 	render() {
 		return (
 			<nav>
 				<div className="nav-wrapper">
-					<a className="left brand-logo">Emaily</a>
+					<a href="/" className="left brand-logo">
+						Emaily
+					</a>
 					<ul className="right">
-						<li>
-							<a>Login with Google</a>
-						</li>
+						{this.renderContent()}
 					</ul>
 				</div>
 			</nav>
@@ -17,4 +36,8 @@ class Header extends Component {
 	}
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
